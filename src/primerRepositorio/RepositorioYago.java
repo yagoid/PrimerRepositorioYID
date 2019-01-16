69d8dc000c8f2bc0c1b100fa7ae0a8811685a8a9
21836404 <21836404@N205-24.alumnos.uem.es>
2019-01-16 10:39:50 +0100

    PrimerRepositorioInicial

---
+++ src/primerRepositorio/RepositorioYago.java
@@ -0,0 +1,11 @@
+package primerRepositorio;
+
+public class RepositorioYago {
+
+	public static void main(String[] args) {
+		System.out.println("Primer repositorio para GitHub y "
+				+ "Creado por Yago Iglesias");
+
+	}
+
+}
